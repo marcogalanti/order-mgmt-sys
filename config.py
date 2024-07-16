94cd3624ba488dd6c3d9fd478b1ba625646bf7d6
marcogalanti <marco.galanti@gmail.com>
2024-07-16 22:09:35 +0000

    second config correction due to db exposure

--- config.py
+++ config.py
@@ -7,9 +7,12 @@ import os
 app = Flask(__name__)
 
 # Configurazione del database
+db_name = os.environ["DB_NAME"]
+db_host = os.environ["DB_HOST"]
+db_port = os.environ["DB_PORT"]
 db_user = os.environ["DB_USER"]
 db_password = os.environ["DB_PASSWORD"]
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{db_user}:{db_password}@cloud-services.demos.mulesoft.com:32692/sedb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inizializza SQLAlchemy
